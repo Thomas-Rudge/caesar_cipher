e7a23d94002d4bca29f51456ec02e1ec9dee5324
Thomas-Rudge <thomas.rudge85@gmail.com>
2016-11-16 14:12:05 +0000

    Refactored
    It is slightly more verbose, but more efficient and reusable.

--- caesar.rb
+++ caesar.rb
@@ -1,20 +1,31 @@
 def cipher(text, shift)
   return text if text == '' || shift == 0
   
-  shift = shift % 95
+  shift %= 95
   
   text = text.split('').map do |char|
-    if char.ord.between?(0, 31) || char.ord == 127
+    char = char.ord
+    
+    if !character_printable?(char)
       char = nil
       next
     end
     
-    char = char.ord + shift
-    char = char > 126 ? (char % 126) + 31 : char
-    char = char < 32  ? 126 - (31 - char) : char
+    char += shift
+    
+    if char > 126
+      char = (char % 126) + 31
+    elsif char < 32
+      char = 126 - (31 - char)
+    end
+    
     char = char.chr
   end.join
   
   return text
   
+end
+
+def character_printable?(char)
+  char.between?(32, 126)
 end
