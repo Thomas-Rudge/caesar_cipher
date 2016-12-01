814147179ebc72b4721623b5da920b6ee694187c
Thomas-Rudge <thomas.rudge85@gmail.com>
2016-12-01 18:05:24 +0000

    Refactored

--- caesar.rb
+++ caesar.rb
@@ -1,31 +1,31 @@
 def cipher(text, shift)
   return text if text == '' || shift == 0
-  
+
   shift %= 95
-  
+
   text = text.split('').map do |char|
     char = char.ord
-    
-    unless character_printable?(char)
-      char = nil
-      next
-    end
-    
+    char = character_printable?(char)
+
+    char.nil? ? next : nil
+
     char += shift
-    
+
     if char > 126
       char = (char % 126) + 31
     elsif char < 32
       char = 126 - (31 - char)
     end
-    
+
     char = char.chr
   end.join
-  
+
   return text
-  
+
 end
 
 def character_printable?(char)
-  char.between?(32, 126)
+  char = nil unless char.between?(32, 126)
+
+  char
 end
